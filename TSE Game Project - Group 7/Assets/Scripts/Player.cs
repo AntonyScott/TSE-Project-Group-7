bf7e37aee1dd593ba7b02615b7a3b447e3b2a347
Jonathan <jon.byrne27@btinternet.com>
2022-02-20 20:09:50 +0000

    Player Prefab added, as well as scripts for player movement and camera following player

--- TSE Game Project - Group 7/Assets/Scripts/Player.cs
+++ TSE Game Project - Group 7/Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Vector2 movement;
+    public float speed;
+    public Rigidbody2D playerRb;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,11 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal"); //gets the horizontal part for movement
+        movement.y = Input.GetAxisRaw("Vertical"); //gets the vertical part of movement 
+    }
+    private void FixedUpdate()
+    {
+        playerRb.MovePosition(playerRb.position + movement * speed * Time.fixedDeltaTime); //player moves the the position dictated by movement
     }
 }
