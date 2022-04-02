531c6907d735ddb11d86f3b0dff92b88579eda82
Antony <antonyscott1@hotmail.co.uk>
2022-04-02 18:44:19 +0100

    Commit 13
    Fixed merge changes from Ellis's previous commit.

--- TSE Game Project - Group 7/Assets/Scripts/PlayerScripts/PlayerHealth.cs
+++ TSE Game Project - Group 7/Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -26,17 +26,14 @@ public class PlayerHealth : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-<<<<<<< Updated upstream
         if (collision.tag == "HealthPickup")
         {
             GainHealth(30);
             Destroy(collision.gameObject);
         }
 
-        if (collision.tag == "Enemy")
-=======
+        //if (collision.tag == "Enemy")
         if (collision.tag == "EnemyBullet")
->>>>>>> Stashed changes
         {
             TakeDamage(10);
             Destroy(collision.gameObject);
